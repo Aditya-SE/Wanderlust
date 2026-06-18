d6c6b6030f90c4b560e70ea15a2255cae8d06aac
Aditya-SE <adityapatil849703801313@gmail.com>
2026-06-18 08:53:57 +0530

    added styling to notification - success

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
@@ -28,7 +28,7 @@ router.post(
 
     await listing.save();
     await newReview.save();
-
+    req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`);
   })
 );
@@ -40,6 +40,7 @@ router.delete(
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review Deleted!");
     res.redirect(`/listings/${id}`);
   })
 );
