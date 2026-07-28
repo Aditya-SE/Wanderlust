ef9c37c9bbb97ce6942c367e1a357e93964551e0
Aditya-SE <adityapatil849703801313@gmail.com>
2026-07-29 00:05:37 +0530

    fixed edit location

--- controllers/listings.js
+++ controllers/listings.js
@@ -67,14 +67,24 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
+
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  listing.geometry = response.body.features[0].geometry;
 
   if (typeof req.file !== "undefined") {
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename };
-    await listing.save();
   }
+
+  await listing.save();
 
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
